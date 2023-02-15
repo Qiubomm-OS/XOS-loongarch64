b6264fe4a9d859762cbf895ab1ad5f3e67c38f3e
huloves <17791627852@163.com>
2023-02-16 01:02:43 +0800

    add efistub.h

--- driver/firmware/efi/libstub/efi-stub-entry.c
+++ driver/firmware/efi/libstub/efi-stub-entry.c
@@ -1,3 +1,5 @@
 #include <linux/efi.h>
 
-efi_status_t efi_pe_entry(efi_handle_t handle);
+efi_status_t efi_pe_entry(efi_handle_t handle, efi_system_table_t *systab)
+{
+}
--- include/linux/types.h
+++ include/linux/types.h
@@ -7,14 +7,18 @@ typedef enum {
 } bool;
 
 typedef unsigned char		u8;
+typedef u8			__u8;
 typedef unsigned short		u16;
 typedef unsigned int		u32;
 typedef unsigned long long	u64;
+typedef u64			__u64;
 
 typedef signed short		s16;
 
 typedef unsigned long		size_t;
 typedef u64			phys_addr_t;
+
+#define __aligned_u64 __u64 __attribute__((aligned(8)))
 
 typedef struct {
 	__u8 b[16];
