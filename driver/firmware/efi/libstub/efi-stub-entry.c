f9df1d9697abc8f703f374bcbbc813a01c26f8eb
huloves <17791627852@163.com>
2023-02-24 23:49:14 +0800

    update

--- driver/firmware/efi/libstub/efi-stub-entry.c
+++ driver/firmware/efi/libstub/efi-stub-entry.c
@@ -5,6 +5,34 @@
 
 #include "efistub.h"
 
+/* Is this type a native word size -- useful for atomic operations */
+#define __native_word(t) \
+	(sizeof(t) == sizeof(char) || sizeof(t) == sizeof(short) || \
+	 sizeof(t) == sizeof(int) || sizeof(t) == sizeof(long))
+
+# define __compiletime_assert(condition, msg, prefix, suffix) do { } while (0)
+
+#define _compiletime_assert(condition, msg, prefix, suffix) \
+	__compiletime_assert(condition, msg, prefix, suffix)
+
+#define compiletime_assert(condition, msg) \
+	_compiletime_assert(condition, msg, __compiletime_assert_, __COUNTER__)
+
+#define compiletime_assert_rwonce_type(t)					\
+	compiletime_assert(__native_word(t) || sizeof(t) == sizeof(long long),	\
+		"Unsupported access size for {READ,WRITE}_ONCE().")
+
+#define __WRITE_ONCE(x, val)						\
+do {									\
+	*(volatile typeof(x) *)&(x) = (val);				\
+} while (0)
+
+#define WRITE_ONCE(x, val)						\
+do {									\
+	compiletime_assert_rwonce_type(x);				\
+	__WRITE_ONCE(x, val);						\
+} while (0)
+
 efi_status_t __efiapi efi_pe_entry(efi_handle_t handle, efi_system_table_t *systab)
 {	
 	efi_loaded_image_t *image;
@@ -18,7 +46,11 @@ efi_status_t __efiapi efi_pe_entry(efi_handle_t handle, efi_system_table_t *syst
 	unsigned long reserve_size = 0;
 
 	// WRITE_ONCE(efi_system_table, systab);
-	memcpy(&efi_system_table, systab, sizeof(efi_system_table));
+	// memcpy(efi_system_table, systab, sizeof(efi_system_table_t));
+	// *(volatile efi_system_table_t *)&efi_system_table = systab;
+	*(volatile typeof(efi_system_table) *)&(efi_system_table) = (systab);
+	// __WRITE_ONCE(efi_system_table, systab);
+
 
 	/* Check if we were booted by the EFI firmware */
 	if (efi_system_table->hdr.signature != EFI_SYSTEM_TABLE_SIGNATURE)
--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -14,26 +14,4 @@ typedef __u16 __attribute__((__may_alias__)) __u16_alias_t;
 typedef __u32 __attribute__((__may_alias__)) __u32_alias_t;
 typedef __u64 __attribute__((__may_alias__)) __u64_alias_t;
 
-static __always_inline void __write_once_size(volatile void *p, void *res, int size)
-{
-	switch (size) {
-	case 1: *(__u8_alias_t  *) res = *(volatile __u8_alias_t  *) p; break;
-	case 2: *(__u16_alias_t *) res = *(volatile __u16_alias_t *) p; break;
-	case 4: *(__u32_alias_t *) res = *(volatile __u32_alias_t *) p; break;
-	case 8: *(__u64_alias_t *) res = *(volatile __u64_alias_t *) p; break;
-	default:
-		barrier();
-		memcpy((void *)res, (const void *)p, size);
-		barrier();
-	}
-}
-
-#define WRITE_ONCE(x, val)				\
-({							\
-	union { typeof(x) __val; char __c[1]; } __u =	\
-		{ .__val = (val) }; 			\
-	__write_once_size(&(x), __u.__c, sizeof(x));	\
-	__u.__val;					\
-})
-
 #endif /* _LINUX_VOMPILER_H */
