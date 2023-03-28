ef58f0f6b625b02ef827b95f5028753317c32662
huloves <17791627852@163.com>
2023-03-29 00:04:01 +0800

    add ns16550a driver

--- drivers/serial/ns16550a.c
+++ drivers/serial/ns16550a.c
@@ -1,3 +1,4 @@
+#include <asm-generic/io.h>
 #include <linux/types.h>
 
 /* 串口寄存器偏移地址 */
@@ -11,6 +12,9 @@
 #define UART_MSR     6       // 调制解调器状态寄存器
 #define UART_SR      7       // Scratch Register
 
+#define UART_DIV_LATCH_LOW  0   // 分频锁存器1
+#define UART_DIV_LATCH_HIGH 1   // 分频锁存器2
+
 /* 线路控制寄存器 (LCR) */
 #define UART_LCR_DLAB  0x80  // Divisor Latch Bit
 
@@ -22,30 +26,46 @@
 uint16_t divisor = 0;       // 用于存储波特率分频系数
 
 /* 初始化串口设备 */
-// void serial_init(uint32_t base_addr, uint32_t baud_rate)
-// {
-// 	uint16_t divisor_value = 0;
-//    	uint8_t lcr_value = 0;
+void serial_init(uint32_t base_addr, uint32_t baud_rate)
+{
+	uint16_t divisor_value = 0;
+   	uint8_t lcr_value = 0;
 
-// 	/* 计算波特率分频系数 */
-// 	divisor_value = 115200 / baud_rate;
-// 	divisor = divisor_value;
+	/* 计算波特率分频系数 */
+	divisor_value = 115200 / baud_rate;
+	divisor = divisor_value;
 
-// 	/* 禁用中断 */
-// 	outb(0x00, base_addr + UART_IER);
+	/* 禁用中断 */
+	outb(0x00, base_addr + UART_IER);
 
-// 	/* 设置波特率 */
-// 	lcr_value = inb(base_addr + UART_LCR);
-// 	outb(lcr_value | UART_LCR_DLAB, base_addr + UART_LCR);
-// 	outb(divisor_value & 0xff, base_addr + UART_DIV_LATCH_LOW);
-// 	outb((divisor_value >> 8) & 0xff, base_addr + UART_DIV_LATCH_HIGH);
-// 	outb(lcr_value, base_addr + UART_LCR);
+	/* 设置波特率 */
+	lcr_value = inb(base_addr + UART_LCR);
+	outb(lcr_value | UART_LCR_DLAB, base_addr + UART_LCR);
+	outb(divisor_value & 0xff, base_addr + UART_DIV_LATCH_LOW);
+	outb((divisor_value >> 8) & 0xff, base_addr + UART_DIV_LATCH_HIGH);
+	outb(lcr_value, base_addr + UART_LCR);
 
-// 	/* 设置数据位、停止位、校验位等参数 */
-// 	outb(0x03, base_addr + UART_LCR);   // 8 数据位，1 停止位，无校验
-// 	outb(0xc7, base_addr + UART_MCR);   // 打开 DTR、RTS 和 OUT2 信号
+	/* 设置数据位、停止位、校验位等参数 */
+	outb(0x03, base_addr + UART_LCR);   // 8 数据位，1 停止位，无校验
+	outb(0xc7, base_addr + UART_MCR);   // 打开 DTR、RTS 和 OUT2 信号
 
-// 	/* 清空串口缓冲区 */
-// 	while (inb(base_addr + UART_LSR) & UART_LSR_DR)
-// 		inb(base_addr + UART_RX);
-// }
+	/* 清空串口缓冲区 */
+	while (inb(base_addr + UART_LSR) & UART_LSR_DR)
+		inb(base_addr + UART_RX);
+}
+
+/* 发送一个字符 */
+void serial_putc(uint32_t base_addr, char c)
+{
+    while ((inb(base_addr + UART_LSR) & UART_LSR_THRE) == 0)
+        ;  // 等待发送缓冲区为空
+    outb(c, base_addr + UART_TX);
+}
+
+/* 接收一个字符 */
+char serial_getc(uint32_t base_addr)
+{
+    while ((inb(base_addr + UART_LSR) & UART_LSR_DR) == 0)
+        ;  // 等待接收缓冲区非空
+    return inb(base_addr + UART_RX);
+}
