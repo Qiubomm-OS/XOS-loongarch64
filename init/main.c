b0ce359b7081b1f4aa0696bc7c4c0069f0d60ce4
huloves <17791627852@163.com>
2023-05-26 01:32:36 +0800

    更新，并查看如何获得CPU标识

--- init/main.c
+++ init/main.c
@@ -14,11 +14,16 @@ union thread_union init_thread_union;
 void __init __no_sanitize_address start_kernel(void)
 {
 	char str[] = "XOS-loongarch64";
-	local_irq_disable();
+
 	serial_ns16550a_init(0x1fe001e0, 9600);
 	printk("%s %s-%d.%d.%d\n", "hello", str, 0, 0, 1);
 
+	local_irq_disable();
 	early_boot_irqs_disabled = true;
+
+	/**
+	 * 禁止中断，进行必要的设置后开启
+	 */
 
 	while(1);
 }
