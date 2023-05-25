0600d1a577a9f7ad8681cfca605e32d9528a41dd
huloves <17791627852@163.com>
2023-05-25 22:11:32 +0800

    update

--- init/main.c
+++ init/main.c
@@ -4,7 +4,10 @@
 #include <linux/task.h>
 #include <linux/init.h>
 #include <linux/ns16550a.h>
+#include <linux/types.h>
 #include <asm/stdio.h>
+
+bool early_boot_irqs_disabled;
 
 union thread_union init_thread_union;
 
@@ -14,5 +17,8 @@ void __init __no_sanitize_address start_kernel(void)
 	local_irq_disable();
 	serial_ns16550a_init(0x1fe001e0, 9600);
 	printk("%s %s-%d.%d.%d\n", "hello", str, 0, 0, 1);
+
+	early_boot_irqs_disabled = true;
+
 	while(1);
 }
