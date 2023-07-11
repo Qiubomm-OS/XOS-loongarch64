126d2f557be9252b5820f1921bfa0d068515133f
huloves <17791627852@163.com>
2023-07-12 01:03:05 +0800

    整理代码

--- arch/x86/kernel/boot.c
+++ arch/x86/kernel/boot.c
@@ -166,23 +166,6 @@ void __init kern_entry()
 {
 	uint32_t kern_stack_top;
 
-	uint8_t *input = (uint8_t *)0xB8000;
-	uint8_t color = (0 << 4) | (15 & 0x0F);
-
-	*input++ = 'H'; *input++ = color;
-	*input++ = 'e'; *input++ = color;
-	*input++ = 'l'; *input++ = color;
-	*input++ = 'l'; *input++ = color;
-	*input++ = 'o'; *input++ = color;
-	*input++ = ','; *input++ = color;
-	*input++ = ' '; *input++ = color;
-	*input++ = 'O'; *input++ = color;
-	*input++ = 'U'; *input++ = color;
-	*input++ = 'R'; *input++ = color;
-	*input++ = 'O'; *input++ = color;
-	*input++ = 'S'; *input++ = color;
-	*input++ = '.'; *input++ = color;
-
 	gdt_create();
 	page_create();
 	
